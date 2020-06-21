b7b5fc7b2b655fbb9f7c18f58ef8b6ffc3081373
Luiryna <luiryna322@gmail.com>
2020-06-21 23:08:38 +0300

    dao factory added

--- src/main/java/dao/DaoFactory.java
+++ src/main/java/dao/DaoFactory.java
@@ -1,17 +1,33 @@
 package dao;
+import dao.impl.BankAccountDaoImpl;
+import dao.impl.BankCardDaoImpl;
+import dao.impl.ClientDaoImpl;
 
 public class DaoFactory {
 
     private static final DaoFactory instance = new DaoFactory();
 
+    private BankAccountDao bankAccountDao = new BankAccountDaoImpl();
+    private BankCardDao bankCardDao = new BankCardDaoImpl();
+    private ClientDao clientDao = new ClientDaoImpl();
 
     private DaoFactory() {
 
     }
 
-    private static DaoFactory getInstance() {
+    public static DaoFactory getInstance() {
         return instance;
     }
 
+    public BankCardDao getBankCardDao() {
+        return bankCardDao;
+    }
 
+    public BankAccountDao getBankAccountDao() {
+        return bankAccountDao;
+    }
+
+    public ClientDao getClientDao() {
+        return clientDao;
+    }
 }
